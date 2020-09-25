2ebf062165120ed3d0f58a4aa45008aedfdc73e8
quarkusio <no-reply@quarkus.io>
2020-09-25 00:09:29 +0000

    Initial commit

---
+++ src/test/java/br/com/fiap/challenge/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.fiap.challenge;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
